18bfb85f24337286a6e9dab2813647742dfe1875
agusmarmor <amarmor06@gmail.com>
2022-10-24 21:44:18 -0400

    added less or more python version

--- python/Less or More.py
+++
@@ -1,7 +0,0 @@
-def main():
-    numsAsString = input("").split()
-    numsAsInt = [int(x) for x in numsAsString]
-
-
-
-main()
---
+++ python/LessOrMore.py
@@ -0,0 +1,13 @@
+def main():
+    numsAsString = input("Enter count, integers separated by a space: ").split()
+    nums = [int(x) for x in numsAsString[1:]]
+    count = 0
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] > nums[j]:
+                count += 1
+                break
+    print(count)
+
+
+main()
